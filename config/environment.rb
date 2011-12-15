c586d94b17959068bc3705b85cce003859dc6b37
Michael Dunagan <omnidreamer@gmail.com>
2011-12-15 04:03:12 -0700

    adding entire project

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+# Initialize the rails application
+Magicsite::Application.initialize!
+#config.action_mailer.delivery_method = :smtp | :sendmail | :test
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magicsite::Application.config.secret_token = 'c502dadab6a0f0bb2f287c3242169162aaa476e5541a7b699772d46ef99e815106790401da41622b06fb312912e2362d5db571e13c7e3c170cf3dd759ac844e4'
